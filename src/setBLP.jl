ac801ebfc0eb2caf67a9c6e766e49c5e1766250a
ArieBeresteanu <arie@pitt.edu>
2023-11-29 17:21:55 -0500

    Update setBLP.jl

--- src/setBLP.jl
+++ src/setBLP.jl
@@ -12,7 +12,7 @@ include("vertices.jl")
 export Vertex, subVertex, addVertex, lambdaVertex, negVertex #, xangle, fetchX, fetchY
 
 include("segments.jl") 
-export Segment, dotDist, xangle
+export Segment, dotDist #, xangle
 
 include("polygons.jl")  
 export Polygon, minkowskiSum, lambdaPolygon, dirHausdorff, hausdorff, sortPolygon!, angles, scatterPolygon, plotPolygon
